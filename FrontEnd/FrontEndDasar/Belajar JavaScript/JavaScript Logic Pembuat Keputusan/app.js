c62e97d1b34c856028643663af93bbc7e352aaa3
ariemax <aribprasetyo88@gmail.com>
2023-01-25 08:25:58 +0700

    add if statement angka kecil atau besar

--- FrontEnd/FrontEndDasar/Belajar JavaScript/JavaScript Logic Pembuat Keputusan/app.js
+++ FrontEnd/FrontEndDasar/Belajar JavaScript/JavaScript Logic Pembuat Keputusan/app.js
@@ -12,3 +12,12 @@
 
 let angka = Math.random();
 console.log(angka);
+
+if (angka >= 0.5) {
+    console.log("Angka lebih besar dari 0.5");
+}
+
+if (angka <= 0.5) {
+    console.log("Angka lebih kecil dari 0.5");
+}
+
