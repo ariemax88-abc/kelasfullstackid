aee12ac71df1508e60f3959a6b4a7aa237ffc84e
ariemax <aribprasetyo88@gmail.com>
2023-01-25 08:47:27 +0700

    add case nilai to if statement

--- FrontEnd/FrontEndDasar/Belajar JavaScript/JavaScript Logic Pembuat Keputusan/app.js
+++ FrontEnd/FrontEndDasar/Belajar JavaScript/JavaScript Logic Pembuat Keputusan/app.js
@@ -21,11 +21,24 @@
 //     console.log("Angka lebih kecil dari 0.5");
 // }
 
-const iniHari = 'Sabtu';
+// const iniHari = 'Sabtu';
 
-if(iniHari === 'Senin'){
-    console.log("Selamat bekerja kawan!");
-}  else if (iniHari === 'Sabtu') {
-    console.log("Semoga jangan cepat berlalu!");
+// if(iniHari === 'Senin'){
+//     console.log("Selamat bekerja kawan!");
+// }  else if (iniHari === 'Sabtu') {
+//     console.log("Semoga jangan cepat berlalu!");
+// }
+
+const nilai = '30';
+
+if (nilai < 50) {
+    console.log('E');
+} else if (nilai < 60) {
+    console.log('D');
+} else if (nilai < 70) {
+    console.log('C');
+} else if (nilai < 80) {
+    console.log('B');
+} else if (nilai < 100) {
+    console.log('A');
 }
-
