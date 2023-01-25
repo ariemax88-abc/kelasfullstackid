88c37443d645b982a08183f061fe3be385d7545a
ariemax <aribprasetyo88@gmail.com>
2023-01-25 08:36:51 +0700

    add if else statement case hari

--- FrontEnd/FrontEndDasar/Belajar JavaScript/JavaScript Logic Pembuat Keputusan/app.js
+++ FrontEnd/FrontEndDasar/Belajar JavaScript/JavaScript Logic Pembuat Keputusan/app.js
@@ -10,14 +10,22 @@
 
 // console.log("setelah pembanding");
 
-let angka = Math.random();
-console.log(angka);
+// let angka = Math.random();
+// console.log(angka);
 
-if (angka >= 0.5) {
-    console.log("Angka lebih besar dari 0.5");
-}
+// if (angka >= 0.5) {
+//     console.log("Angka lebih besar dari 0.5");
+// }
 
-if (angka <= 0.5) {
-    console.log("Angka lebih kecil dari 0.5");
+// if (angka <= 0.5) {
+//     console.log("Angka lebih kecil dari 0.5");
+// }
+
+const iniHari = 'Sabtu';
+
+if(iniHari === 'Senin'){
+    console.log("Selamat bekerja kawan!");
+}  else if (iniHari === 'Sabtu') {
+    console.log("Semoga jangan cepat berlalu!");
 }
 
