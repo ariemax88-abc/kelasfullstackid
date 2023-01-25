e5e5c4e1fcfa275d66278549e0b5e723f50374f7
ariemax <aribprasetyo88@gmail.com>
2023-01-25 07:57:14 +0700

    add 2 console statement

--- FrontEnd/FrontEndDasar/Belajar JavaScript/JavaScript Logic Pembuat Keputusan/app.js
+++ FrontEnd/FrontEndDasar/Belajar JavaScript/JavaScript Logic Pembuat Keputusan/app.js
@@ -1,3 +1,11 @@
-alert(1+1);
+// alert(1+1);
 
-console.log('hello world');
+// console.log('hello world');
+
+console.log("sebelum pembanding")
+
+if (1 + 1 === 3) {
+    console.log('di dalam pembanding');
+}
+
+console.log("setelah pembanding");
