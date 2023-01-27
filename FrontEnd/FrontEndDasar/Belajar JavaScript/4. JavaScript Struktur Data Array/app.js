b253eaf0ca43ff67a348328cab7af45cd6adf9a8
ariemax <aribprasetyo88@gmail.com>
2023-01-28 05:58:58 +0700

    add array join

--- FrontEnd/FrontEndDasar/Belajar JavaScript/4. JavaScript Struktur Data Array/app.js
+++ FrontEnd/FrontEndDasar/Belajar JavaScript/4. JavaScript Struktur Data Array/app.js
@@ -91,15 +91,26 @@
 // console.log(animals.slice());
 // // Expected output: Array ["ant", "bison", "camel", "duck", "elephant"]
 
-const array1 = ['one', 'two', 'three'];
-console.log('array1:', array1);
-// Expected output: "array1:" Array ["one", "two", "three"]
+// const array1 = ['one', 'two', 'three'];
+// console.log('array1:', array1);
+// // Expected output: "array1:" Array ["one", "two", "three"]
 
-const reversed = array1.reverse();
-console.log('reversed:', reversed);
-// Expected output: "reversed:" Array ["three", "two", "one"]
+// const reversed = array1.reverse();
+// console.log('reversed:', reversed);
+// // Expected output: "reversed:" Array ["three", "two", "one"]
 
-// Careful: reverse is destructive -- it changes the original array.
-console.log('array1:', array1);
-// Expected output: "array1:" Array ["three", "two", "one"]
+// // Careful: reverse is destructive -- it changes the original array.
+// console.log('array1:', array1);
+// // Expected output: "array1:" Array ["three", "two", "one"]
+
+const elements = ['Fire', 'Air', 'Water'];
+
+console.log(elements.join());
+// Expected output: "Fire,Air,Water"
+
+console.log(elements.join(''));
+// Expected output: "FireAirWater"
+
+console.log(elements.join('-'));
+// Expected output: "Fire-Air-Water"
 
