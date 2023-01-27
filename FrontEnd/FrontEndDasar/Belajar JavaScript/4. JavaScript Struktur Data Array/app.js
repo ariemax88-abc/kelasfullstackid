672a7a5c907944b069e88fd3d06a395c88eb0a06
ariemax <aribprasetyo88@gmail.com>
2023-01-27 09:01:57 +0700

    add shift and unshift di array

--- FrontEnd/FrontEndDasar/Belajar JavaScript/4. JavaScript Struktur Data Array/app.js
+++ FrontEnd/FrontEndDasar/Belajar JavaScript/4. JavaScript Struktur Data Array/app.js
@@ -4,18 +4,25 @@
 // warna[3] = "merah muda"
 // console.log(warna);
 
-let barbel = [];
+// let barbel = [];
 
-barbel.push(20);
+// barbel.push(20);
 
-barbel.push(15);
+// barbel.push(15);
 
-barbel.push(10, 5, 2.5);
+// barbel.push(10, 5, 2.5);
 
-barbel.pop();
-barbel.pop();
-barbel.pop();
-barbel.pop();
-barbel.pop();
+// barbel.pop();
+// barbel.pop();
+// barbel.pop();
+// barbel.pop();
+// barbel.pop();
 
-console.log(barbel);
+// console.log(barbel);
+
+let student = ['Bobby', 'Charlie', 'Denies', 'Eula'];
+student.unshift('Ain');
+student.unshift(0);
+student.shift();
+
+console.log(student);
