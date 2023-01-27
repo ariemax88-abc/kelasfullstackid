53fced9e12823e7bcd69b0df616a935c857886ac
ariemax <aribprasetyo88@gmail.com>
2023-01-27 07:50:43 +0700

    add array index push case

--- FrontEnd/FrontEndDasar/Belajar JavaScript/4. JavaScript Struktur Data Array/app.js
+++ FrontEnd/FrontEndDasar/Belajar JavaScript/4. JavaScript Struktur Data Array/app.js
@@ -1,5 +1,15 @@
-let warna = ['hijau', 'kuning', 'kelab', 'bu'];
+// let warna = ['hijau', 'kuning', 'kelab', 'bu'];
 
-warna[2] = "kelabu"
-warna[3] = "merah muda"
-console.log(warna);
+// warna[2] = "kelabu"
+// warna[3] = "merah muda"
+// console.log(warna);
+
+let barbel = [];
+
+barbel.push(20);
+
+barbel.push(15);
+
+barbel.push(10, 5, 2.5);
+
+console.log(barbel);
