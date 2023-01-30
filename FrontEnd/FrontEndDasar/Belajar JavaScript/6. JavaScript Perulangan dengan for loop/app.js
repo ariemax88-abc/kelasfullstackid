e86e2f5e5d6685877776c5089c5c0d1ce01938ac
ariemax <aribprasetyo88@gmail.com>
2023-01-30 08:33:59 +0700

    add loop use for sederhana

--- FrontEnd/FrontEndDasar/Belajar JavaScript/6. JavaScript Perulangan dengan for loop/app.js
+++ FrontEnd/FrontEndDasar/Belajar JavaScript/6. JavaScript Perulangan dengan for loop/app.js
@@ -1,11 +1,16 @@
 // looping manual :v
-console.log(1)
-console.log(2)
-console.log(3)
-console.log(4)
-console.log(5)
-console.log(6)
-console.log(7)
-console.log(8)
-console.log(9)
-console.log(10)
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log(6)
+// console.log(7)
+// console.log(8)
+// console.log(9)
+// console.log(10)
+
+for (let i = 1; i <= 10; i++){
+    console.log('saya ada di dalam loop');
+    console.log(i);
+}
