81b8d768b393fa39be1367e8e0681967db1c92b8
ariemax <aribprasetyo88@gmail.com>
2023-01-31 08:24:42 +0700

    get data dari nested array - not display

--- FrontEnd/FrontEndDasar/Belajar JavaScript/6. JavaScript Perulangan dengan for loop/app.js
+++ FrontEnd/FrontEndDasar/Belajar JavaScript/6. JavaScript Perulangan dengan for loop/app.js
@@ -37,10 +37,21 @@
 //     console.log(i+1, animals[i]);
 // }
 
-const pilihan = 'abcd';
-for (let i = 1; i <= 10; i++) {
-    console.log(`${i}. Soal nomor ${i}:`)
-    for (let j = 0; j < pilihan.length; j++) {
-        console.log(`   ${pilihan[j]}. Pilihan Jawaban`);
-    }
+const studentRow = [
+    ['Olivia', 'Lian', 'Emma', 'Noah'],
+    ['Amelia', 'Oliver', 'Ava', 'Elijah'],
+    ['Sophia', 'Mateo', 'Isabella', 'Lucas'],
+];
+
+for (let i = 0; i < studentRow.length; i++){
+    const row = studentRow(i);
+    console.log(row);
 }
+
+// const pilihan = 'abcd';
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`${i}. Soal nomor ${i}:`)
+//     for (let j = 0; j < pilihan.length; j++) {
+//         console.log(`   ${pilihan[j]}. Pilihan Jawaban`);
+//     }
+// }
