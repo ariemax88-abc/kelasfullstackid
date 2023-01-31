bbbd4dae849c2ef5e610e47476dadff88195a0d2
ariemax <aribprasetyo88@gmail.com>
2023-01-31 10:39:37 +0700

    add game tebak angka in while

--- FrontEnd/FrontEndDasar/Belajar JavaScript/6. JavaScript Perulangan dengan for loop/app.js
+++ FrontEnd/FrontEndDasar/Belajar JavaScript/6. JavaScript Perulangan dengan for loop/app.js
@@ -99,3 +99,24 @@
 //     console.log(i);
 //     if(i === 100) break;
 // }
+
+// 9. Membuat Game Tebak Tebakkan Angka Dengan While
+let maximum = parseInt(prompt('Masukkan nilai maksimal!'));
+while (!maximum) {
+    maximum = parseInt(prompt('Masukkan nilai maksimal!'));
+}
+
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+console.log(targetNum);
+
+let guess = parseInt(prompt('Isi tebakan pertama kamu!'));
+
+while (parseInt(guess) !== targetNum) {
+    if (guess > targetNum) {
+        guess = parseInt(prompt('Terlalu tinggi! tebak lagi:'));
+    } else {
+        guess = parseInt(prompt('Terlalu rendah! tebak lagi:'));
+    }
+}
+
+alert('Selamat Tebakan Anda Benar!!');
