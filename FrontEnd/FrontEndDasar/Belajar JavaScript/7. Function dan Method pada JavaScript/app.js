c434f901c1480fa3461c2c570d87e2d408c2d0e9
ariemax <aribprasetyo88@gmail.com>
2023-02-01 08:31:24 +0700

    running function basic

--- FrontEnd/FrontEndDasar/Belajar JavaScript/7. Function dan Method pada JavaScript/app.js
+++ FrontEnd/FrontEndDasar/Belajar JavaScript/7. Function dan Method pada JavaScript/app.js
@@ -1,6 +1,18 @@
 // 1. Pengenalan Function Di Javascript
 // let lemparDadu = console.log(lemparDadu);
 
+//2. Mendefinisikan Dan Menjalankan Function
 function lemparDadu(){
     console.log(Math.floor(Math.random() * 6 ) + 1);
 }
+function nyanyi() {
+    console.log("Cek...");
+    console.log("Do re mi...");
+}
+
+// panggil fungsi 
+nyanyi();
+nyanyi();
+nyanyi();
+nyanyi();
+nyanyi();
