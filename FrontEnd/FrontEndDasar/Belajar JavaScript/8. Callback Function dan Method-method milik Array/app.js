1791dc0249fa1ac248247f4f62203d653f6de4c9
ariemax <aribprasetyo88@gmail.com>
2023-02-04 00:25:41 +0700

    case 1 - callback function map

--- FrontEnd/FrontEndDasar/Belajar JavaScript/8. Callback Function dan Method-method milik Array/app.js
+++ FrontEnd/FrontEndDasar/Belajar JavaScript/8. Callback Function dan Method-method milik Array/app.js
@@ -35,30 +35,70 @@
 
 // case 4
 
-const animes = [
-    {
-        title: 'Attact on Titan',
-        rating: 90,
-    },
-    {
-        title: 'One Piece',
-        rating: 89,
-    },
-    {
-        title: 'Bleach',
-        rating: 82,
-    },
-    {
-        title: 'Hunter x Hunter',
-        rating: 90,
-    },
-    {
-        title: 'Naruto',
-        rating: 84,
-    },
+// const animes = [
+//     {
+//         title: 'Attact on Titan',
+//         rating: 90,
+//     },
+//     {
+//         title: 'One Piece',
+//         rating: 89,
+//     },
+//     {
+//         title: 'Bleach',
+//         rating: 82,
+//     },
+//     {
+//         title: 'Hunter x Hunter',
+//         rating: 90,
+//     },
+//     {
+//         title: 'Naruto',
+//         rating: 84,
+//     },
 
-];
+// ];
 
-animes.forEach(function(anime){
-    console.log(`${anime.title} - ${anime.rating}/100`);
+// animes.forEach(function(anime){
+//     console.log(`${anime.title} - ${anime.rating}/100`);
+// });
+
+
+// 2. Map Method
+
+// case 1
+
+const angka = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const angkaDouble = angka.map(function(num){
+    return num * 2;
 });
+
+// case 2 
+
+// const animes = [
+//     {
+//         title: 'Attact on Titan',
+//         rating: 90,
+//     },
+//     {
+//         title: 'One Piece',
+//         rating: 89,
+//     },
+//     {
+//         title: 'Bleach',
+//         rating: 82,
+//     },
+//     {
+//         title: 'Hunter x Hunter',
+//         rating: 90,
+//     },
+//     {
+//         title: 'Naruto',
+//         rating: 84,
+//     },
+
+// ];
+
+// const animeList = animes.map(function(anime){
+//     return anime.title.toUpperCase();
+// });
