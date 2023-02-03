3242619251aa5247d2bc849b2ab52c41a0833168
ariemax <aribprasetyo88@gmail.com>
2023-02-03 23:55:32 +0700

    case 4 - use callback function list anime

--- FrontEnd/FrontEndDasar/Belajar JavaScript/8. Callback Function dan Method-method milik Array/app.js
+++ FrontEnd/FrontEndDasar/Belajar JavaScript/8. Callback Function dan Method-method milik Array/app.js
@@ -25,40 +25,40 @@
 
 // case 3
 
-const angka = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const angka = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-angka.forEach(function(el){
-    if(el % 2 === 0 ) {
-        console.log(el);
-    }
-});
+// angka.forEach(function(el){
+//     if(el % 2 === 0 ) {
+//         console.log(el);
+//     }
+// });
 
 // case 4
 
-// const animes = [
-//     {
-//         title: 'Attact on Titan',
-//         rating: 90,
-//     },
-//     {
-//         title: 'One Piece',
-//         rating: 89,
-//     },
-//     {
-//         title: 'Bleach',
-//         rating: 82,
-//     },
-//     {
-//         title: 'Hunter x Hunter',
-//         rating: 90,
-//     },
-//     {
-//         title: 'Naruto',
-//         rating: 84,
-//     },
+const animes = [
+    {
+        title: 'Attact on Titan',
+        rating: 90,
+    },
+    {
+        title: 'One Piece',
+        rating: 89,
+    },
+    {
+        title: 'Bleach',
+        rating: 82,
+    },
+    {
+        title: 'Hunter x Hunter',
+        rating: 90,
+    },
+    {
+        title: 'Naruto',
+        rating: 84,
+    },
 
-// ];
+];
 
-// animes.forEach(function(anime){
-//     console.log(`${anime.title} - ${anime.rating}/100`);
-// });
+animes.forEach(function(anime){
+    console.log(`${anime.title} - ${anime.rating}/100`);
+});
