8cc163fe4c2ad2b2550a4e67f547634ccb31a7e6
ariemax <aribprasetyo88@gmail.com>
2023-02-05 23:34:04 +0700

    [8] case 1 : no use reduce method

--- FrontEnd/FrontEndDasar/Belajar JavaScript/8. Callback Function dan Method-method milik Array/app.js
+++ FrontEnd/FrontEndDasar/Belajar JavaScript/8. Callback Function dan Method-method milik Array/app.js
@@ -234,33 +234,126 @@
 
 // case 3 - anime use some
 
-const animes = [
-    {
-        title: 'Attact on Titan',
-        rating: 90,
-        year: 2006
-    },
-    {
-        title: 'One Piece',
-        rating: 89,
-        year: 2022,
-    },
-    {
-        title: 'Bleach',
-        rating: 82,
-        year: 2020,
-    },
-    {
-        title: 'Hunter x Hunter',
-        rating: 90,
-        year: 2016,
-    },
-    {
-        title: 'Naruto',
-        rating: 84,
-        year: 2002,
-    },
+// const animes = [
+//     {
+//         title: 'Attact on Titan',
+//         rating: 90,
+//         year: 2006
+//     },
+//     {
+//         title: 'One Piece',
+//         rating: 89,
+//         year: 2022,
+//     },
+//     {
+//         title: 'Bleach',
+//         rating: 82,
+//         year: 2020,
+//     },
+//     {
+//         title: 'Hunter x Hunter',
+//         rating: 90,
+//         year: 2016,
+//     },
+//     {
+//         title: 'Naruto',
+//         rating: 84,
+//         year: 2002,
+//     },
 
-];
+// ];
 
-const isAnimeListNew = animes.some((anime) => anime.year > 2010);
+// const isAnimeListNew = animes.some((anime) => anime.year > 2010);
+
+
+// 8. Mendapatkan Satu Nilai Sesuai Kondisi Dari Sebuah Array Dengan Reduce
+ 
+// case 1 - no use reduce method
+const examScore = [80, 85, 70, 90, 93, 77];
+let total = 0;
+for ( let score of examScore){
+    total += score;
+}
+
+// case 2 - use reduce method
+
+// const total = examScore.reduce((total, score) => {
+//     return total + score;
+// });
+
+// case 3 - use reduce in array object
+
+// const animes = [
+//     {
+//         title: 'One Piece',
+//         rating: 89,
+//         year: 2022,
+//     },
+//     {
+//         title: 'Bleach',
+//         rating: 82,
+//         year: 2020,
+//     },
+//     {
+//         title: 'Attact on Titan',
+//         rating: 92,
+//         year: 2006
+//     },
+//     {
+//         title: 'Hunter x Hunter',
+//         rating: 90,
+//         year: 2016,
+//     },
+//     {
+//         title: 'Naruto',
+//         rating: 84,
+//         year: 2002,
+//     },
+
+// ];
+
+// const bestAnime = animes.reduce((bestAnime, currAnime) => {
+//     console.log(bestAnime, currAnime);
+//     if(currAnime.rating > bestAnime.rating) {
+//         return currAnime;
+//     }
+//     return bestAnime;
+// });
+
+// case 4 - ise reduce in low anime
+// const animes = [
+//     {
+//         title: 'One Piece',
+//         rating: 89,
+//         year: 2022,
+//     },
+//     {
+//         title: 'Bleach',
+//         rating: 82,
+//         year: 2020,
+//     },
+//     {
+//         title: 'Attact on Titan',
+//         rating: 92,
+//         year: 2006
+//     },
+//     {
+//         title: 'Hunter x Hunter',
+//         rating: 90,
+//         year: 2016,
+//     },
+//     {
+//         title: 'Naruto',
+//         rating: 84,
+//         year: 2002,
+//     },
+
+// ];
+
+// const lowAnime = animes.reduce((lowAnime, currAnime) => {
+//     console.log(lowAnime, currAnime);
+//     if(currAnime.rating < lowAnime.rating) {
+//         return currAnime;
+//     }
+//     return lowAnime;
+// });
