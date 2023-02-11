992a18864410bc3a73cbf01d0c220ab13837329d
ariemax <aribprasetyo88@gmail.com>
2023-02-11 08:37:58 +0700

    add event use prop in DOM -> onclick , mouseover

--- FrontEnd/FrontEndDasar/Belajar JavaScript DOM/2. Mengenal Event DOM - Membuat Web Lebih Interaktif/app.js
+++ FrontEnd/FrontEndDasar/Belajar JavaScript DOM/2. Mengenal Event DOM - Membuat Web Lebih Interaktif/app.js
@@ -1,6 +1,13 @@
 const button = document.querySelector('#clickme');
 
-// event onclick
+// event onclick use function langsung
 button.onclick = function () {
     console.log("You clicked me");
 };
+
+// pisahkan use variabel, lalu panggil var nya
+const scream = () => {
+    console.log('Dont touch me');
+}
+
+button.onmouseover = scream;
