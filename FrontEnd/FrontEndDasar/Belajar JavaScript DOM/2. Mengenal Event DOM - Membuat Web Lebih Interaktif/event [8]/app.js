7dfbc348c5181049302bf67cc40ed42a1ce0b72b
ariemax <aribprasetyo88@gmail.com>
2023-02-13 10:25:05 +0700

    [10] event listener input dan change

--- FrontEnd/FrontEndDasar/Belajar JavaScript DOM/2. Mengenal Event DOM - Membuat Web Lebih Interaktif/event [8]/app.js
+++ FrontEnd/FrontEndDasar/Belajar JavaScript DOM/2. Mengenal Event DOM - Membuat Web Lebih Interaktif/event [8]/app.js
@@ -1,11 +1,44 @@
+// const form = document.querySelector('#form');
+// form.addEventListener('submit', function(e) {
+//    e.preventDefault();
+//     console.log('submitted form')
+//     // console.log('submited');
+// });
+
+// document.querySelector('a').addEventListener('click', (e) => {
+//     e.preventDefault()
+//     console.log('harusnya ke google')
+// })
+
+
+// [9] Praktek Input Realtime Dengan Event Dan Dom 06:55
+
 const form = document.querySelector('#form');
+const input = document.querySelector('input');
+const list = document.querySelector('#notes');
+
+// [10] Event Input Dan Change Pada Form
+// input.addEventListener('change', (e) => {
+//     console.log('Nilai Berubah')
+// });
+
+input.addEventListener('input', (e) => {
+    document.querySelector('h1').innerText = input.value;
+    console.log('Nilai berhasil diinput');
+});
+
 form.addEventListener('submit', function(e) {
-   e.preventDefault();
-    console.log('submitted form')
+    e.preventDefault();
+    const noteValue = input.value;
+    const newList = document.createElement('li');
+    
+    newList.innerText = noteValue;
+     list.append(newList);
+     input.value = '';
+    // console.log(newList);
+
+   
+    // console.log(noteValue)
     // console.log('submited');
 });
 
-document.querySelector('a').addEventListener('click', (e) => {
-    e.preventDefault()
-    console.log('harusnya ke google')
-})
