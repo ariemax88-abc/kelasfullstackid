d4a9007244970e76c30cf7511a898b164247fa96
ariemax <aribprasetyo88@gmail.com>
2023-02-15 11:36:35 +0700

    [4] styling keep board final

--- FrontEnd/FrontEndDasar/Belajar JavaScript DOM/3. Latihan DOM Membuat Papan Skor/Changes/app.js
+++ FrontEnd/FrontEndDasar/Belajar JavaScript DOM/3. Latihan DOM Membuat Papan Skor/Changes/app.js
@@ -2,11 +2,22 @@ const p1Button = document.querySelector('#p1-button');
 const p2Button = document.querySelector('#p2-button');
 const p1Display = document.querySelector('#p1-display');
 const p2Display = document.querySelector('#p2-display');
+const resetButton = document.querySelector('#reset');
+const winPointOption = document.querySelector('#winpoint');
 
 let p1Score = 0;
 let p2Score = 0;
-let winPoint = 5;
+let winPoint = 3;
 let isGameOver = false;
+
+// [2] add function reset
+function reset() {
+    isGameOver = false;
+    p1Score = 0
+    p2Score = 0
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+}
 
 p1Button.addEventListener('click', function(){
     if(!isGameOver) {
@@ -27,3 +38,8 @@ p2Button.addEventListener('click', function(){
     p2Display.textContent = p2Score;
 });
 
+resetButton.addEventListener('click', reset);
+winPointOption.addEventListener('change', function() {
+    winPoint = parseInt(this.value) 
+    reset();
+});
