fdb6dc4f2701ecb85ab5a4124137143c9adf91a9
ariemax <aribprasetyo88@gmail.com>
2023-02-17 23:34:43 +0700

    [3] callback hell in javascript

--- FrontEnd/FrontEndDasar/ASYNCAWAIT/app.js
+++ FrontEnd/FrontEndDasar/ASYNCAWAIT/app.js
@@ -1,9 +1,42 @@
-console.log('Bakal muncul pertama');
+// console.log('Bakal muncul pertama');
 
-setTimeout(() => {
-    console.log(
-        'ini udah masuk memory proses tapi dijalankan setelah 3 detik kedepan'
+// setTimeout(() => {
+//     console.log(
+//         'ini udah masuk memory proses tapi dijalankan setelah 3 detik kedepan'
+//     );
+// }, 3000);
+
+// console.log('Bakal muncul kedua');
+
+setTimeout(function() {
+    document.body.style.backgroundColor = 'red';
+
+    setTimeout(function() {
+        document.body.style.backgroundColor = 'yellow';
+        setTimeout(function() {
+            document.body.style.backgroundColor = 'green';
+            setTimeout(function() {
+                document.body.style.backgroundColor = 'blue';
+            },2000);
+        },2000);
+    },2000);
+},2000);
+
+searchMoviesApi('naruto', function (movies) {
+    saveToMyDb(movies,
+         function (){
+        // kalau berhasil kita simpan
+        filteringMovie(
+            movies,
+            () => {},
+            () => {} 
+        );
+    },
+    () => {
+        // kalau gagal kita coba lagi
+        }
     );
-}, 3000);
-
-console.log('Bakal muncul kedua');
+},
+() => {
+    // server error
+});
