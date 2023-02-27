c95c49dd97976636dda61a3400746c2f3c90b7ee
ariemax <aribprasetyo88@gmail.com>
2023-02-27 07:34:10 +0700

    [3] Pengenalan JSON

---
+++ FrontEnd/FrontEndDasar/ASYNCAWAIT/AJAXANDAPI/app.js
@@ -0,0 +1,12 @@
+const data = `{
+    "ticker": {
+        "base": "BTC",
+        "target": "IDR",
+        "price": "354716000.00000000",
+        "volume": "36.71951753",
+        "change": "-37000.00000000"
+    },
+    "timestamp": 1677287705,
+    "success": true,
+    "error": ""
+}`;
