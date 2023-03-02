8a5abae3ca595d87ab4a24807a1feecdda82de8a
ariemax <aribprasetyo88@gmail.com>
2023-03-02 09:10:09 +0700

    [11] case 1 - pamggil axios

--- FrontEnd/FrontEndDasar/AJAXANDAPI/ajaxapi/app.js
+++ FrontEnd/FrontEndDasar/AJAXANDAPI/ajaxapi/app.js
@@ -85,19 +85,65 @@
 // loadPeople();
 
 // case 5 - Handle error in Async Await
-const loadPeople = async () => {
-    try {
-    const res = await fetch('https://swapi.dev/api/peopleERROR/1');
-    const data = await res.json();
-    console.log(data);
-    const res2 = await fetch('https://swapi.dev/api/people/2');
-    const data2 = await res2.json();
-    console.log(data2);
-    } catch (err) {
-        // proses lainnya
-        console.log('error', err);
-    }
-};
+// const loadPeople = async () => {
+//     try {
+//     const res = await fetch('https://swapi.dev/api/peopleERROR/1');
+//     const data = await res.json();
+//     console.log(data);
+//     const res2 = await fetch('https://swapi.dev/api/people/2');
+//     const data2 = await res2.json();
+//     console.log(data2);
+//     } catch (err) {
+//         // proses lainnya
+//         console.log('error', err);
+//     }
+// };
 
-loadPeople();
+// loadPeople();
+
+// 11. Req use Library Axios
+
+// case 1: panggil axios
+axios
+    .get('https://swapi.dev/api/people/1')
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+// case 2: get data & tampil error
+// axios
+//     .get('https://swapi.dev/api/people123/1')
+//     .then((res) => {
+//         console.log(res.data);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+
+// case 3: add alert
+// axios
+//     .get('https://swapi.dev/api/people123/1')
+//     .then((res) => {
+//         console.log(res.data);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//         alert(err.message);
+//     });
+
+// case 4: Use Async Await versi Axios
+// const getPeople = async (id) => {
+//     try {
+//         const res = await axios.get(`https://swapi.dev/api/people/${id}`);
+//         console.log(res.data);
+//     } catch (error) {
+//         console.log(error);
+//         console.log(error.message);
+//         console.log(error.response.status);
+//         console.log(error.response.data);
+//     }
+// };
 
